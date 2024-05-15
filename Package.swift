33acf9b9b5022410c1ddbe9a92acc3f55c854316
Peter Liddle <development.peter@liddle.network>
2024-05-14 21:41:36 -0500

    Initial commit

---
+++ Package.swift
@@ -0,0 +1,36 @@
+// swift-tools-version: 5.9.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "langchainlite-swift",
+    platforms: [
+        .iOS(.v15),
+        .macOS(.v12),
+        .watchOS(.v8)
+    ],
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "LangChainLite",
+            targets: ["LangChainLite"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/ptliddle/openai-kit.git", branch: "main")
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "LangChainLite",
+            dependencies: [
+                .product(name: "OpenAIKit", package: "openai-kit")
+            ]
+        
+        ),
+        .testTarget(
+            name: "LangChainTests",
+            dependencies: ["LangChainLite"]),
+    ]
+)
